d78252ffd4e8e7537dff61e91465f6aaf10994e6
Victor Desktop <Victo@DESKTOP-C0LLRRO>
2022-09-18 17:26:43 +0200

    full 3rd person movement and camera implementation

--- Assets/MyAssets/move.cs
+++ Assets/MyAssets/move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,39 +6,56 @@ using UnityEngine.EventSystems;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed;
-    float turnSpeed = 12f;
 
-    public Transform camera;
+    public CharacterController controller;
+    public Animator animator;
+    public Transform cam;
+    public Transform player;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector3 moveDir = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (moveDir.magnitude >= 0.1f)
+        if (!player.GetComponent<PlayerCombat>().isDead())
         {
-            float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed,0.1f);
-            transform.rotation = Quaternion.Euler(0f,angle,0f);
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-            Vector3 dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            animator.SetFloat("Horizontal", horizontalInput);
+            animator.SetFloat("Vertical", verticalInput);
 
 
-            transform.Translate(dir.normalized * Time.deltaTime);
+            if (direction.magnitude >= 0.1f)
+            {
+                animator.SetBool("running", true);
+
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+                if (moveDirection.x > 0)
+                    transform.forward = moveDirection;
+
+                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("running", false);
+            }
         }
-
         
-
-
-
     }
 }
