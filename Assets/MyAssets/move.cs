636a3c7be0c1b8e468b060b5ea19ddfb8f9753b2
Victor Desktop <Victo@DESKTOP-C0LLRRO>
2022-10-31 13:25:28 +0100

    updated movement

--- Assets/MyAssets/move.cs
+++ Assets/MyAssets/move.cs
@@ -6,30 +6,28 @@ using UnityEngine.EventSystems;
 
 public class move : MonoBehaviour
 {
-
+    [SerializeField]
     public CharacterController controller;
+    [SerializeField]
     public Animator animator;
-    public Transform cam;
-    public Transform player;
-
     [SerializeField]
-    public float _speed = 30f;
+    private Transform camera;
     [SerializeField]
-    public float _rotationSpeed = 90f;
-    float _turnSmoothVelocity;
+    public float speed = 5f;
     [SerializeField]
-    private float _gravity = -9.81f;
+    private float gravity = 9.81f;
     [SerializeField]
     private float _jumpSpeed = 3.5f;
 
-    private Vector3 moveVelocity;
-    private Vector3 turnVelocity;
-
+    private float directionY;
     private float targetAngle;
+
+    //public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -37,6 +35,70 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", horizontalInput);
+        animator.SetFloat("Vertical", verticalInput);
+
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        direction = Quaternion.AngleAxis(camera.rotation.eulerAngles.y, Vector3.up) * direction;
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                animator.SetTrigger("Jumping");
+                directionY = _jumpSpeed;
+            }
+        }
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if (verticalInput > 0)
+            {
+                targetAngle = MathF.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+                transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            }
+            directionY -= gravity * Time.deltaTime;
+
+            direction.y = directionY;
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
+
+/*
+ *  float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _rotationSpeed);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+ */
+
+
+/*
+[SerializeField]
+public float _speed = 30f;
+[SerializeField]
+public float _rotationSpeed = 90f;
+float _turnSmoothVelocity;
+[SerializeField]
+private float _gravity = -9.81f;
+[SerializeField]
+private float _jumpSpeed = 3.5f;
+
+private Vector3 moveVelocity;
+private Vector3 turnVelocity;
+
+private float targetAngle;
+*/
+
+/*
+ 
         if (!player.GetComponent<PlayerCombat>().isDead())
         {
             float horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -65,13 +127,4 @@ public class move : MonoBehaviour
             controller.Move(moveVelocity * Time.deltaTime);
             transform.Rotate(turnVelocity * Time.deltaTime);
         }
-    }
-}
-
-/*
- *  float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _rotationSpeed);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
- */
+*/
